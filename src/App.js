493eae45a8a4908669e82ee42bcd80af97fb2525
AB <anchalbhargava86@gmail.com>
2023-01-16 16:59:15 +0530

    Some Experiments with Tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 function App() {
   return (
-   <h1 className="bg-slate-600">Hello</h1> 
+  <>
+    <h1 className="text-lime-900 text-4xl bg-red-500">Hello</h1> 
+    <button className="ring-4 ring-offset-2">Click Me</button>
+  </>
+   
   );
 }
 
