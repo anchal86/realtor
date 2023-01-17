fce6fd4f114f5394b1c26e91e4a0e02109ac8196
AB <anchalbhargava86@gmail.com>
2023-01-17 14:15:19 +0530

    Created Pages and Installed React-Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Offers from './pages/Offers';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
   return (
-  <>
-    <h1 className="text-lime-900 text-4xl bg-red-500">Hello</h1> 
-    <button className="ring-4 ring-offset-2">Click Me</button>
-  </>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
    
   );
 }
